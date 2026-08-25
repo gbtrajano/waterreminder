554b136c7ef89aa4839ef84550f87c24fce570d4
gbtrajano <conversarcomgabriel@gmail.com>
2026-08-24 22:06:49 -0300

    bot update

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
+import time
 import requests
+import schedule
 
+# Configurações do seu bot
 TELEGRAM_TOKEN = "8955435133:AAHA_G_frQF8-dg4Rmo29FTgTAoaqgGWZ90"
-CHAT_ID = "1147312591"
-
+CHAT_ID = "1147312591"  # Pode obter enviando uma mensagem para o bot @userinfobot
 
 def enviar_lembrete():
-  mensagem = "💧 *Hora de beber água!* Mantenha-se hidratado."
-  url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
-  payload = {
-      "chat_id": CHAT_ID,
-      "text": mensagem,
-      "parse_mode": "Markdown",
-  }
-  response = requests.post(url, json=payload)
-  print(f"Status do envio: {response.status_code}")
+    mensagem = "💧 *Hora de beber água!* Mantenha-se hidratado."
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": mensagem,
+        "parse_mode": "Markdown"
+    }
+    requests.post(url, json=payload)
 
+# Agendamentos (Horários desejados)
+schedule.every().day.at("09:00").do(enviar_lembrete)
+schedule.every().day.at("11:00").do(enviar_lembrete)
+schedule.every().day.at("14:00").do(enviar_lembrete)
+schedule.every().day.at("16:00").do(enviar_lembrete)
+schedule.every().day.at("18:00").do(enviar_lembrete)
 
-if __name__ == "__main__":
-  enviar_lembrete()
+print("Bot de lembrete iniciado...")
+
+while True:
+    schedule.run_pending()
+    time.sleep(60)
